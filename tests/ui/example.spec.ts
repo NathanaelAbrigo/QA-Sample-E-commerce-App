f3bdaf87f30723197906202885354cea14d7371c
Nathanael Chris Abrigo <nathanael.abrigo@gmail.com>
2025-06-11 16:22:13 +0800

    test case 3 & 4
    Test Case 3: Login User with incorrect email and password
    Test Case 4: Logout User

--- tests/ui/example.spec.ts
+++ tests/ui/example.spec.ts
@@ -28,7 +28,7 @@ var mobileNumber = "12345456";
 
 // .serial for sequencial execution
 test.describe.serial('Sequential test group', () => {
-  
+
   test('Test Case 1: Register User', async ({ page }) => {
     await page.getByRole('link', { name: 'Signup / Login' }).click();
 
@@ -92,10 +92,48 @@ test.describe.serial('Sequential test group', () => {
     await expect(page).toHaveTitle(/Automation Exercise/);
     await page.getByRole('heading', { name: `Logged in as ${name}` }).isVisible();
 
-    // Delete Account
-    await page.getByRole('link', { name: 'Delete Account' }).click();
-    await page.getByText('Account Deleted!').isVisible();
-    await page.getByRole('link', { name: 'Continue' }).click();
+    // // Delete Account
+    // await page.getByRole('link', { name: 'Delete Account' }).click();
+    // await page.getByText('Account Deleted!').isVisible();
+    // await page.getByRole('link', { name: 'Continue' }).click();
+  });
+
+  test('Test Case 3: Login User with incorrect email and password', async ({ page }) => {
+    await page.getByRole('link', { name: 'Signup / Login' }).click();
+
+    //Signup / Login Page
+    await page.getByRole('heading', { name: 'Login to your account' }).isVisible();
+
+    await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).fill('wrongpassword');
+
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeEnabled();
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('Your email or password is incorrect!')).toBeVisible();
+  });
+
+  test('Test Case 4: Logout User', async ({ page }) => {
+    await page.getByRole('link', { name: 'Signup / Login' }).click();
+
+    //Signup / Login Page
+    await page.getByRole('heading', { name: 'Login to your account' }).isVisible();
+
+    await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).fill(password);
+
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeEnabled();
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Back to Home Page
+    await expect(page).toHaveTitle(/Automation Exercise/);
+    await page.getByRole('heading', { name: `Logged in as ${name}` }).isVisible();
+
+    // Logout
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await expect(page).toHaveTitle(/Automation Exercise/);
   });
 
 });
